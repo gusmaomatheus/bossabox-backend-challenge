973c5fd5d26cdf8df1ce3379f024fb2c5a4b16c0
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 07:02:46 -0300

    feat(ToolService/ToolServiceImpl): create insert method to insert new tools into the database

--- src/main/java/br/com/gusmaomatheus/vuttr/services/tool/ToolService.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/services/tool/ToolService.java
@@ -1,5 +1,9 @@
 package br.com.gusmaomatheus.vuttr.services.tool;
 
+import br.com.gusmaomatheus.vuttr.dtos.ToolDTO;
+import br.com.gusmaomatheus.vuttr.models.Tool;
+
 public interface ToolService {
 
+    Tool insert(ToolDTO data);
 }
--- src/main/java/br/com/gusmaomatheus/vuttr/services/tool/ToolServiceImpl.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/services/tool/ToolServiceImpl.java
@@ -1,5 +1,7 @@
 package br.com.gusmaomatheus.vuttr.services.tool;
 
+import br.com.gusmaomatheus.vuttr.dtos.ToolDTO;
+import br.com.gusmaomatheus.vuttr.models.Tool;
 import br.com.gusmaomatheus.vuttr.repositories.ToolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,9 @@ public class ToolServiceImpl implements ToolService {
     @Autowired
     private ToolRepository repository;
 
+    @Override
+    public Tool insert(ToolDTO data) {
+        Tool entity = new Tool(data.title(), data.link(), data.description(), data.tags());
+        return repository.save(entity);
+    }
 }
