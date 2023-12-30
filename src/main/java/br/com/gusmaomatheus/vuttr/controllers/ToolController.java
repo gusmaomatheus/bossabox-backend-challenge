eb1e57ec83fa2504dddf7470d5d42aa93ce4df95
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 07:04:11 -0300

    feat(ToolController): create route for post mapping

--- src/main/java/br/com/gusmaomatheus/vuttr/controllers/ToolController.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/controllers/ToolController.java
@@ -1,7 +1,13 @@
 package br.com.gusmaomatheus.vuttr.controllers;
 
+import br.com.gusmaomatheus.vuttr.dtos.ToolDTO;
+import br.com.gusmaomatheus.vuttr.models.Tool;
 import br.com.gusmaomatheus.vuttr.services.tool.ToolServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,10 @@ public class ToolController {
     @Autowired
     private ToolServiceImpl service;
 
+    @PostMapping
+    public ResponseEntity<Tool> insert(@RequestBody ToolDTO data) {
+        Tool entity = service.insert(data);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(entity);
+    }
 }
