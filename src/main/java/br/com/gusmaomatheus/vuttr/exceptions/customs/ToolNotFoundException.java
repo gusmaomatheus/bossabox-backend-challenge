b469c4d97771190ba721a48cb9f3392339dcd7c4
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 07:55:04 -0300

    feat: create a custom exception that is called when the tool does not exist

---
+++ src/main/java/br/com/gusmaomatheus/vuttr/exceptions/customs/ToolNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.gusmaomatheus.vuttr.exceptions.customs;
+
+public class ToolNotFoundException extends RuntimeException {
+
+    public ToolNotFoundException(String message) {
+        super(message);
+    }
+}
