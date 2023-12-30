23522c6e40db191d6449df8c7f198113aafc5e3e
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 01:12:59 -0300

    feat: create auth service implementation

---
+++ src/main/java/br/com/gusmaomatheus/vuttr/services/auth/AuthServiceImpl.java
@@ -0,0 +1,18 @@
+package br.com.gusmaomatheus.vuttr.services.auth;
+
+import br.com.gusmaomatheus.vuttr.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
