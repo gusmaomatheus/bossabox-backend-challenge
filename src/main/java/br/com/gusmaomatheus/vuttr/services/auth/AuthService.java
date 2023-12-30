0b9bef9c3c87a90b40b0d4c0a4fbc74e8fa3d5c9
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 01:12:40 -0300

    feat: create auth service

---
+++ src/main/java/br/com/gusmaomatheus/vuttr/services/auth/AuthService.java
@@ -0,0 +1,6 @@
+package br.com.gusmaomatheus.vuttr.services.auth;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface AuthService extends UserDetailsService {
+}
