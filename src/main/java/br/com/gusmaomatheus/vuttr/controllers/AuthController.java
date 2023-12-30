a4c2c37c92c454d535752d552f6e7c0e082dda92
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 17:28:37 -0300

    feat: create authorization controller

---
+++ src/main/java/br/com/gusmaomatheus/vuttr/controllers/AuthController.java
@@ -0,0 +1,9 @@
+package br.com.gusmaomatheus.vuttr.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vuttr/auth")
+public class AuthController {
+}
