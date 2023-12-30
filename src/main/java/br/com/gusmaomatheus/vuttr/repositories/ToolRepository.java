3a65e9f4d8c601f0c5b109d5e2cff48f35114dad
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 08:54:50 -0300

    feat(ToolRepository): create findByTag to search for a tools by a common tag

--- src/main/java/br/com/gusmaomatheus/vuttr/repositories/ToolRepository.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/repositories/ToolRepository.java
@@ -2,8 +2,15 @@ package br.com.gusmaomatheus.vuttr.repositories;
 
 import br.com.gusmaomatheus.vuttr.models.Tool;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Repository
 public interface ToolRepository extends JpaRepository<Tool, Long> {
+
+    @Query(value = "SELECT tool FROM Tool tool WHERE :tag MEMBER OF tool.tags")
+    List<Tool> findByTag(@RequestParam String tag);
 }
