28ad051c42c9380ce0fe5e60255ec76a684b8a19
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 01:17:27 -0300

    feat(UserRepository): create findByUsername method to search for a user by their username

--- src/main/java/br/com/gusmaomatheus/vuttr/repositories/UserRepository.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/repositories/UserRepository.java
@@ -2,11 +2,12 @@ package br.com.gusmaomatheus.vuttr.repositories;
 
 import br.com.gusmaomatheus.vuttr.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-
+    UserDetails findByUsername(String username);
 }
