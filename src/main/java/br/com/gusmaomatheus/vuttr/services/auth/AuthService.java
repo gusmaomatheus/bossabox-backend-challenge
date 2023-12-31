a247d9910da0596c37588cdf7a2bde5843d3782b
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-30 21:32:46 -0300

    feat(AuthService/AuthServiceImpl): create a register method

--- src/main/java/br/com/gusmaomatheus/vuttr/services/auth/AuthService.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/services/auth/AuthService.java
@@ -1,6 +1,9 @@
 package br.com.gusmaomatheus.vuttr.services.auth;
 
+import br.com.gusmaomatheus.vuttr.dtos.auth.register.UserRegisterRequest;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface AuthService extends UserDetailsService {
+
+    void register(UserRegisterRequest data);
 }
--- src/main/java/br/com/gusmaomatheus/vuttr/services/auth/AuthServiceImpl.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/services/auth/AuthServiceImpl.java
@@ -1,5 +1,9 @@
 package br.com.gusmaomatheus.vuttr.services.auth;
 
+import br.com.gusmaomatheus.vuttr.config.security.SecurityConfig;
+import br.com.gusmaomatheus.vuttr.dtos.auth.register.UserRegisterRequest;
+import br.com.gusmaomatheus.vuttr.exceptions.customs.auth.UsernameAlreadyExistsException;
+import br.com.gusmaomatheus.vuttr.models.User;
 import br.com.gusmaomatheus.vuttr.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,9 +14,18 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     @Autowired
     private UserRepository repository;
+    @Autowired
+    private SecurityConfig securityConfig;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return this.repository.findByUsername(username);
+    }
+
+    public void register(UserRegisterRequest request) {
+        String encryptedPassword = securityConfig.passwordEncoder().encode(request.password());
+        User user = new User(request.username(), encryptedPassword, request.role());
+
+        repository.save(user);
     }
 }
