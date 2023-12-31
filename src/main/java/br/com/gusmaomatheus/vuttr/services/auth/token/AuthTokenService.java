65746611b7336d39ae7a43f47329f21e4745c09b
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-31 02:48:13 -0300

    feat(AuthTokenService/AuthTokenServiceImpl): create method to generate a token

--- src/main/java/br/com/gusmaomatheus/vuttr/services/auth/token/AuthTokenService.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/services/auth/token/AuthTokenService.java
@@ -1,5 +1,8 @@
 package br.com.gusmaomatheus.vuttr.services.auth.token;
 
+import br.com.gusmaomatheus.vuttr.models.User;
+
 public interface AuthTokenService {
 
+    String generateToken(User user);
 }
