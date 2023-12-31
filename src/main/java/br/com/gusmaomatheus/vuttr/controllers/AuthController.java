ad5831d9f09322e0f4cafe29f473d94ef57aeaf4
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-31 02:59:30 -0300

    feat(AuthController): create a login endpoint

--- src/main/java/br/com/gusmaomatheus/vuttr/controllers/AuthController.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package br.com.gusmaomatheus.vuttr.controllers;
 
+import br.com.gusmaomatheus.vuttr.dtos.auth.login.UserLoginRequest;
 import br.com.gusmaomatheus.vuttr.dtos.auth.register.UserRegisterRequest;
-import br.com.gusmaomatheus.vuttr.dtos.auth.UserAuthResponse;
+import br.com.gusmaomatheus.vuttr.dtos.auth.login.UserLoginResponse;
+import br.com.gusmaomatheus.vuttr.dtos.auth.register.UserRegisterResponse;
 import br.com.gusmaomatheus.vuttr.services.auth.AuthServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,11 +21,20 @@ public class AuthController {
     private AuthServiceImpl service;
 
     @PostMapping("/register")
-    public ResponseEntity<UserAuthResponse> register(@RequestBody @Valid UserRegisterRequest request) {
+    public ResponseEntity<UserRegisterResponse> register(@RequestBody @Valid UserRegisterRequest request) {
         service.register(request);
 
-        UserAuthResponse response = new UserAuthResponse(HttpStatus.CREATED, String.format("Successful registering user '%s'!",  request.username()));
+        UserRegisterResponse response = new UserRegisterResponse(HttpStatus.CREATED, String.format("Successful registering user '%s'!",  request.username()));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLoginResponse> login(@RequestBody @Valid UserLoginRequest request) throws Exception {
+        String token = service.login(request);
+
+        UserLoginResponse response = new UserLoginResponse(HttpStatus.OK, String.format("Successful logging in to user '%s'", request.username()), token);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
