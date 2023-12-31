c6918e7ec2b741a1fbc2d0831cba4538605793d5
Matheus Gusmão <mgusmao412@gmail.com>
2023-12-31 02:52:41 -0300

    feat(auth): implement a filter in SecurityConfig

--- src/main/java/br/com/gusmaomatheus/vuttr/config/security/SecurityConfig.java
+++ src/main/java/br/com/gusmaomatheus/vuttr/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.gusmaomatheus.vuttr.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -23,11 +27,14 @@ public class SecurityConfig {
                 .csrf(CsrfConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/vuttr/auth/*").permitAll()
                         .requestMatchers(HttpMethod.POST, "/vuttr/tools/*").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/vuttr/tools/*").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "vuttr/tools/*").hasRole("ADMIN")
                         .anyRequest().authenticated()
-                ).build();
+                )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
